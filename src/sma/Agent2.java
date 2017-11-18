f552bcc95c3cfbe813758cf86a4d2c03cb00a3fd
massi <mr.hadjsaid@hotmail.fr>
2017-11-18 15:09:46 +0100

    ajout deplacement intelligent v1

--- src/sma/Agent2.java
+++ src/sma/Agent2.java
@@ -6,17 +6,16 @@ import exploration.Case;
 import exploration.Vehicule;
 
 /**
- * Classe Agent, elle definit dedant le traitement que les agent font 
- * sur la carte et cela via les méthodes sedeplacer(), sedeplacer2(), sedeplacer3()
+ * Classe Agent, elle definit dedant le traitement que les agent font sur la
+ * carte et cela via les méthodes sedeplacer(), sedeplacer2(), sedeplacer3()
  * 
  * @author MASSI
  *
  */
 public class Agent2 extends Thread {
-	
-	
-private Vehicule voiture; 
-	
+
+	private Vehicule voiture;
+
 	// Constructure
 	public Agent2(Vehicule voiture) {
 		this.voiture = voiture;
@@ -30,20 +29,20 @@ private Vehicule voiture;
 	/**
 	 * Méthode run() , lancement des agents
 	 */
-	
+
 	public void run() {
 		while (!Thread.currentThread().isInterrupted()) {
 			try {
-			sedeplacer1();
-			//sedeplacer2();
-			//sedeplacer3();
+				sedeplacer1();
+				// sedeplacer2();
+				// sedeplacer3();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	public void  sedeplacer1() throws InterruptedException {
+
+	public void sedeplacer1() throws InterruptedException {
 
 		ArrayList<Case> voisins = this.voiture.getVoisinage();
 		System.out.println(voisins.size());
@@ -51,7 +50,6 @@ private Vehicule voiture;
 		ArrayList<Case> memoire = new ArrayList<>();
 		int i = (int) (Math.random() * voisins.size());
 		voisin = voisins.get(i);
-		
 
 		int v = (int) (Math.random() * 4);
 		boolean bool = true;
@@ -86,79 +84,85 @@ private Vehicule voiture;
 					// DEPLACEMENT Aléatoire
 					if (v == 0) {
 						boolean bool1 = true;
-						
-						while (bool1){
-						this.voiture.avancer(-1, -1);
-						sleep(1);
-						ArrayList<Case> voisins2 = this.voiture.getVoisinage();
-						for (int j = 0; j<voisins2.size();j++)
-						{
-							if (voisins2.get(j).isDecouverte()== false||voisins2.size()<24)
-							{
-								bool1 = false;
+
+						while (bool1) {
+
+							if (voisin.isOccupee()) {
 								break;
-								
+							}
+
+							this.voiture.avancer(-1, -1);
+							sleep(100);
+							ArrayList<Case> voisins2 = this.voiture.getVoisinage();
+							for (int j = 0; j < voisins2.size(); j++) {
+								if (voisins2.get(j).isDecouverte() == false || voisins2.size() < 24 || voisins2.get(j).isOccupee()) {
+									bool1 = false;
+									break;
+
+								}
 							}
 						}
-						}
-					}
-					else if (v == 1) {
+					} else if (v == 1) {
 						boolean bool1 = true;
-						while (bool1){
+						while (bool1) {
+							if (voisin.isOccupee()) {
+								break;
+							}
 							this.voiture.avancer(1, 1);
-							sleep(1);
+							sleep(100);
 							ArrayList<Case> voisins2 = this.voiture.getVoisinage();
-							for (int j = 0; j<voisins2.size();j++)
-							{
-								if (voisins2.get(j).isDecouverte()== false ||voisins2.size()<24 )
-								{
+							for (int j = 0; j < voisins2.size(); j++) {
+								if (voisins2.get(j).isDecouverte() == false || voisins2.size() < 24 || voisins2.get(j).isOccupee()) {
 									bool1 = false;
 									break;
-									
+
 								}
 							}
-							}
-					}
-					else if (v == 2) {
+						}
+					} else if (v == 2) {
 						boolean bool1 = true;
-						while (bool1){
+						while (bool1) {
+
+							if (voisin.isOccupee()) {
+								break;
+							}
+
 							this.voiture.avancer(-1, 1);
-							sleep(1);
+							sleep(100);
 							ArrayList<Case> voisins2 = this.voiture.getVoisinage();
-							for (int j = 0; j<voisins2.size();j++)
-							{
-								if (voisins2.get(j).isDecouverte()== false||voisins2.size()<24)
-								{
+							for (int j = 0; j < voisins2.size(); j++) {
+								if (voisins2.get(j).isDecouverte() == false || voisins2.size() < 24 || voisins2.get(j).isOccupee()) {
 									bool1 = false;
 									break;
-									
+
 								}
 							}
-							}
-					}
-					else if (v == 3) {
+						}
+					} else if (v == 3) {
 						boolean bool1 = true;
-						while (bool1){
+						while (bool1) {
+							if (voisin.isOccupee()) {
+								break;
+							}
 							this.voiture.avancer(1, -1);
-							sleep(1);
+							sleep(100);
 							ArrayList<Case> voisins2 = this.voiture.getVoisinage();
-							for (int j = 0; j<voisins2.size();j++)
-							{
-								if (voisins2.get(j).isDecouverte()== false||voisins2.size()<24)
-								{
+							for (int j = 0; j < voisins2.size(); j++) {
+								if (voisins2.get(j).isDecouverte() == false || voisins2.size() < 24 ||voisins2.get(j).isOccupee()) {
 									bool1 = false;
 									break;
-									
+
 								}
 							}
-							}
+						}
 					}
 					bool = false;
+					sleep(100);
 				}
 			}
 		}
 
-		 sleep(1);
+		sleep(100);
 	}
 
 }
